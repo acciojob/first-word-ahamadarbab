190bce1e9f997b3a96395c7cdfbea7ebe4e99a2a
Arbab Ahamad <ahmadarbab.456@gmail.com>
2026-03-12 23:49:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function firstWord(s) {
-  // your code here
+  // remove leading spaces
+  s = s.trimStart();
+  
+  // find first space
+  let index = s.indexOf(" ");
+  
+  // if no space exists return entire string
+  if (index === -1) {
+    return s;
+  }
+  
+  // return characters before the first space
+  return s.substring(0, index);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
